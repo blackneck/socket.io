cf49c9bcc6dd63bdc0e87f0d053c28e9f1dec1df
Aliaksandr Charnashei <a.charnashei@icx.local>
2016-06-08 11:09:37 +0300

    allow first peer to wait other connections

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -11,13 +11,31 @@ server.listen(port, function () {
 
 app.use(express.static(__dirname + '/public'));
 
+var offers = [];
+var clients = [];
 
 io.on('connection', function (socket) {
   console.log('connected');
+  clients.push(socket);
+  console.log(clients.length);
+
+  if (offers.length > 0 && clients.length > 1) {
+    socket.emit('offerRecieved', { desc: offers.pop() });
+  }
+
+  socket.on('disconnect', function () {
+    console.log('disconnected');
+    var i = clients.indexOf(socket);
+    clients.splice(i, 1);
+    offers = [];
+  })
 
   socket.on('offer', function (data) {
+    offers.push(data.desc);
     console.log(data.desc.type);
-    socket.broadcast.emit('offerRecieved', { desc: data.desc });
+    if (clients.length > 1) {
+      socket.broadcast.emit('offerRecieved', { desc: offers.pop() });
+    }
   });
 
   socket.on('answer', function (data) {
@@ -26,8 +44,8 @@ io.on('connection', function (socket) {
   });
 
   socket.on('candidate', function (data) {
-    console.log(data.candidate);
     socket.broadcast.emit('candidateRecieved', { candidate: data.candidate });
   });
 
 });
+
